a70b423abc1c75eea30ddff006d1ea7db1dbb3e7
aakashdeveloper <ahanda205@gmail.com>
2023-09-17 09:39:39 +0530

    angular walkthrough

--- fullstack/src/app/app.component.ts
+++ fullstack/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+   // template:`<h1>Hii From developer Funnel</h1>`
+   templateUrl:'./app.component.html'
 })
-export class AppComponent {
-  title = 'fullstack';
-}
+
+export class AppComponent{}
--- fullstack/src/app/app.module.ts
+++ fullstack/src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './Footer/Footer.component';
+import { HeaderComponent } from './Header/header.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './home/search/search.component';
+import { QuickSearchComponent } from './home/quick-search/quick-search.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    // All the component & pipes
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        FooterComponent,
+        HomeComponent,
+        SearchComponent,
+        QuickSearchComponent
+    ],
+
+    // All the modules
+    imports: [
+        BrowserModule
+    ],
+
+    // All the services
+    providers:[],
+
+    // only and only main component
+    bootstrap: [
+        AppComponent
+    ]
 })
-export class AppModule { }
+
+export class AppModule {}
